bfd8eb704c945411b47b1193df89f80d7e68cb00
strongdoctor <strongdoctor@live.com>
2022-02-19 18:05:15 +0200

    Improve UI, change config format

--- anti-afk-mover.py
+++ anti-afk-mover.py
@@ -1,3 +1,4 @@
+from enum import Enum
 from pynput.keyboard import (
     Key,
     Listener as KeyboardListener,
@@ -13,11 +14,25 @@ import time
 import sys
 import threading
 import os
-import json
+import configparser
+import ctypes
 
 if __name__ == "__main__":
-    with open("config.json", "r") as f:
-        config = json.load(f)
+    os.system("mode con: cols=70 lines=15")
+    kernel32 = ctypes.windll.kernel32
+    kernel32.SetConsoleMode(kernel32.GetStdHandle(-10), 128)
+
+    configParser = configparser.ConfigParser()
+    configParser.read("config.toml")
+
+    config = configParser["DEFAULT"]
+
+    startKey = config["startkey"].strip('"')
+    exitKey = config["exitKey"].strip('"')
+
+    class Path(Enum):
+        HOME = 0
+        RUNNING = 1
 
     def clearConsole():
         command = "clear"
@@ -25,17 +40,30 @@ if __name__ == "__main__":
             command = "cls"
         os.system(command)
 
-    def mainMenu():
-        clearConsole()
+    def clearLines(lines: int):
+        sys.stdout.write("\r\033[K")
+        for _ in range(lines):
+            sys.stdout.write("\033[A\r\033[K")
+        sys.stdout.flush()
+
+    def printCurrLine(string: str):
+        sys.stdout.write("\r\033[K" + string)
+        sys.stdout.flush()
+
+    def appHeader():
         print("Anti AFK Mover", flush=True)
         print("Simulates input to avoid getting kicked for being AFK", flush=True)
         print("-------------------------", flush=True)
-        print("[s] - start/stop", flush=True)
-        print("[c] - configuration", flush=True)
-        print("[e] - exit", flush=True)
-        print("", flush=True)
+
+    def mainMenu():
+        print(f"[{startKey}] - start/stop")
+        exitKeys = ", ".join({"e", exitKey})
+        print(f"[{exitKeys}] - exit")
+        sys.stdout.flush()
 
     run = False
+
+    path = Path.HOME
 
     def loopThread():
         global run
@@ -62,17 +90,16 @@ if __name__ == "__main__":
 
             m.move(-randx, -randy)
 
-            sleep_time = randrange(config["minClickTime"], config["maxClickTime"])
+            sleep_time = randrange(
+                int(config["minclicktime"]), int(config["maxclicktime"])
+            )
 
             for x in range(sleep_time, 0, -1):
                 if run == False:
                     return
 
-                clearConsole()
-                mainMenu()
-
                 if run == True:
-                    print(f"> Running.. (Waiting for {x} seconds)", flush=True)
+                    printCurrLine("> Running.. (Waiting for " + str(x) + " seconds)")
 
                 time.sleep(1)
                 if run == False:
@@ -80,23 +107,31 @@ if __name__ == "__main__":
 
     def on_press(key):
         global run
+        global path
         try:
             if key.char == "e":
-                print("> Exiting..", flush=True)
+                print("\n> Exiting..", flush=True)
+                run = False
+                # Give time for thread to gracefully shut down
+                time.sleep(2)
                 return False
-            elif key.char == "c":
-                mainMenu()
-                print("> Not implemented yet :(", flush=True)
-            elif key.char == "s":
+
+            elif key.char == startKey:
                 if run == False:
+                    path = Path.RUNNING
+                    clearLines(3)
                     mainMenu()
-                    print("> Running..", flush=True)
+                    sys.stdout.write("\r\033[K\n")
+                    printCurrLine("> Running..")
                     run = True
                     threading.Thread(target=loopThread).start()
                 else:
+                    path = Path.HOME
                     run = False
+                    clearLines(3)
                     mainMenu()
-                    print("> Idle..", flush=True)
+                    sys.stdout.write("\r\033[K\n")
+                    printCurrLine("> Idle..")
 
         except AttributeError:
             return
@@ -104,7 +139,9 @@ if __name__ == "__main__":
     kblistener = KeyboardListener(on_press=on_press)
     kblistener.start()
 
+    appHeader()
     mainMenu()
-    print("> Idle..", flush=True)
+    print("")
+    printCurrLine("> Idle..")
 
     kblistener.join()
